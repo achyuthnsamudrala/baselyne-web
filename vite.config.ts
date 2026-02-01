2b60215b2d1751bf3d69fca33a86129a090b84ad
Achyuth Samudrala <achyuth.1995@gmail.com>
2026-02-01 21:49:12 +0000

    base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/baselyne-web/" : "/",
+  base: "/baselyne-web/",
   server: {
     host: "::",
     port: 8080,
