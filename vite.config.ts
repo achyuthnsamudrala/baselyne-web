90b4b27f14ce36f265f66446aee2bdfb9ce5f4cf
Achyuth Samudrala <achyuth.1995@gmail.com>
2026-02-01 21:33:39 +0000

    path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/baselyne-web/", // ADD THIS LINE
+  base: mode === "production" ? "/baselyne-web/" : "/",
   server: {
     host: "::",
     port: 8080,
